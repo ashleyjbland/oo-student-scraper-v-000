11453a8d26123c4c309d8e79d678456686bb7b16
Ashley Bland <ashleyjbland@gmail.com>
2018-10-31 22:20:35 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,7 +30,7 @@ class Scraper
     student[:profile_quote]= profile_html.css("div.profile-quote").text.strip
     student[:bio]= profile_html.css("div.bio-content div.description-holder").text.strip
     #profile_html.css("div.social-icon-container a").each do |k, v|
-    students
+    student
    end
 
 
